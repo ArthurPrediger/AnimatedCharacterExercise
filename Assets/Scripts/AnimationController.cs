3f961c3b7b7cd613c506f13e89813c0960bce776
Arthur Rambo Prediger <ArthurPrediger@edu.unisinos.br>
2024-11-04 16:57:52 -0300

    Animations improved.

--- Assets/Scripts/AnimationController.cs
+++ Assets/Scripts/AnimationController.cs
@@ -7,42 +7,30 @@ public class AnimationController : MonoBehaviour
     private Animator ani;
     private bool crouching = false;
 
-    public Transform rootBone;
     private Vector3 initialPosition;
 
     // Start is called before the first frame update
     void Start()
     {
-        ani= GetComponent<Animator>();
-
-        initialPosition = rootBone.position;
+        ani = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        if(Input.GetKeyDown(KeyCode.Space))
+
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            ani.SetTrigger("backFlip");  
+            ani.SetTrigger("backFlip");
         }
-        if(Input.GetKeyDown(KeyCode.C))
+        if (Input.GetKeyDown(KeyCode.C))
         {
             crouching = !crouching;
             ani.SetBool("crouching", crouching);
         }
-        if(Input.GetKeyDown(KeyCode.V))
+        if (Input.GetKeyDown(KeyCode.V))
         {
             ani.SetTrigger("dodge");
         }
-
-        if (ani.GetCurrentAnimatorStateInfo(0).IsName("Dodge"))
-        { 
-            // Calculate the relative position change
-            Vector3 deltaPosition = rootBone.position - initialPosition; 
-            // Move the character to the new position
-            transform.position += deltaPosition;
-            // Update initial position for the next frame
-            initialPosition = rootBone.position; }
-        }
+    }
 }
